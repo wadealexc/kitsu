99a6b34568df74b746928f8ec5164fec412c9b19
wadealexc <pragma-services@proton.me>
2025-12-15 21:32:17 +0000

    feat(tool): add loadWebPage tool

---
+++ src/tools/loadWebpage.ts
@@ -0,0 +1,78 @@
+import { z } from '../config.js';
+
+import type { Tool, ToolContext } from './types.js';
+import type { Browser } from '../browser/browser.js';
+
+const MAX_PAGES_ALLOWED = 3;
+
+const InputSchema = z.object({
+    urls: z.array(z.url({
+        protocol: /^https?$/,
+        hostname: z.regexes.domain
+    }))
+}).describe(`a list of up to ${MAX_PAGES_ALLOWED} web pages to fetch content from`);
+
+const OutputSchema = z.array(z.object({
+    url: z.string(),
+    content: z.string(),
+})).describe(`an array of loaded web pages. if any pages fail to load, they will not be included in the output.`);
+
+type Input = z.infer<typeof InputSchema>;
+type Output = z.infer<typeof OutputSchema>;
+
+class LoadWebpage implements Tool<Input, Output> {
+
+    browser: Browser;
+
+    defaultResultCount: number = 3;
+
+    constructor(browser: Browser) {
+        this.browser = browser;
+    }
+
+    name(): string {
+        return 'loadWebpage';
+    }
+
+    description(): string {
+        return (
+`If the user provides a URL and specifically requests you to load a webpage, use this tool to fetch that page's content.`
+        );
+    }
+
+    strict(): boolean {
+        return false;
+    }
+
+    inputSchema(): z.ZodType<Input> {
+        return InputSchema;
+    }
+
+    outputSchema(): z.ZodType<Output> {
+        return OutputSchema;
+    }
+
+    async call(input: Input): Promise<Output> {
+        const urls: URL[] = input.urls.slice(0, MAX_PAGES_ALLOWED).map(url => new URL(url));
+        const results: Output = [];
+
+        (await Promise.allSettled(this.browser.fetchContent(false, ...urls))).forEach(result => {
+            if (result.status === 'fulfilled') {
+                results.push({
+                    content: result.value.content,
+                    url: result.value.metadata.source.toString(),
+                });
+            } else {
+                console.log(`/tools/loadWebpage: rejected promise: ${result.reason}`);
+            }
+        });
+
+        return results;
+    }
+}
+
+export default function loadWebpage(ctx: ToolContext): Tool<Input, Output> {
+    if (!ctx.browser) throw new Error(`loadWebpage: no browser available`);
+
+    return new LoadWebpage(ctx.browser);
+}
--- src/tools/loader.ts
+++ src/tools/loader.ts
@@ -2,6 +2,7 @@ import type { Tool, ToolFactory } from './types.js';
 
 import webSearch from './webSearch.js';
 import systemInfo from './systemInfo.js';
+import loadWebpage from './loadWebpage.js';
 
 function toGeneric<In, Out>(factory: ToolFactory<In, Out>): ToolFactory<unknown, unknown> {
     return factory as ToolFactory<unknown, unknown>;
@@ -11,5 +12,6 @@ export default function loadTools(): ToolFactory<unknown, unknown>[] {
     return [
         toGeneric(systemInfo),
         toGeneric(webSearch),
+        toGeneric(loadWebpage),
     ];
 }
